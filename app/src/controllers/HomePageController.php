1ff8af7823c5518947a584a9831e9bbeb224faab
Ke Wang <wangke97@outlook.com>
2020-03-15 23:47:01 +1300

    Slider

--- app/src/controllers/HomePageController.php
+++ app/src/controllers/HomePageController.php
@@ -8,46 +8,46 @@ use SilverStripe\Control\Session;
 
 class HomePageController extends PageController
 {
-
-    private static $allowed_actions = [];
-
-    public function init()
-    {
-        parent::init();
-        Session::set('FixedImages', FALSE);
-    }
-
-    public function isHome()
-    {
-        return true;
-    }
-
-    public function StoryPages()
-    {
-
-        $ids = [];
-
-        foreach ($this->StoryCategories() as $category) {
-            $ids[] = $category->ID;
-        }
-
-        return StoryPage::get()
-            ->where('StoryCategoryID IN (' . implode(', ', $ids) . ')')
-            ->where('DisplayOrder = 0')
-            ->sort('RAND()');
-    }
-
-    public function getOrderedStories()
-    {
-        $ids = [];
-
-        foreach ($this->StoryCategories() as $category) {
-            $ids[] = $category->ID;
-        }
-
-        return StoryPage::get()
-            ->where('StoryCategoryID IN (' . implode(', ', $ids) . ')')
-            ->where('DisplayOrder != 0');
-        //->sort('RAND()');
-    }
+//
+//    private static $allowed_actions = [];
+//
+//    public function init()
+//    {
+//        parent::init();
+//        Session::set('FixedImages', FALSE);
+//    }
+//
+//    public function isHome()
+//    {
+//        return true;
+//    }
+//
+//    public function StoryPages()
+//    {
+//
+//        $ids = [];
+//
+//        foreach ($this->StoryCategories() as $category) {
+//            $ids[] = $category->ID;
+//        }
+//
+//        return StoryPage::get()
+//            ->where('StoryCategoryID IN (' . implode(', ', $ids) . ')')
+//            ->where('DisplayOrder = 0')
+//            ->sort('RAND()');
+//    }
+//
+//    public function getOrderedStories()
+//    {
+//        $ids = [];
+//
+//        foreach ($this->StoryCategories() as $category) {
+//            $ids[] = $category->ID;
+//        }
+//
+//        return StoryPage::get()
+//            ->where('StoryCategoryID IN (' . implode(', ', $ids) . ')')
+//            ->where('DisplayOrder != 0');
+//        //->sort('RAND()');
+//    }
 }
--- app/templates/App/Page/Layout/HomePage.ss
+++ app/templates/App/Page/Layout/HomePage.ss
@@ -1,4 +1,11 @@
-<div class="content">
-    <h1>Welcome to {{name}}!</h1>
-    <div>$Content</div>
+<div class="slider">
+
+<%--    <div class="slider_logo">--%>
+<%--        <img src="">--%>
+<%--    </div>--%>
+<%--    <div class="slider_container">--%>
+<%--        <div class="info"></div>--%>
+<%--        <button></button>--%>
+<%--    </div>--%>
+    123
 </div>
